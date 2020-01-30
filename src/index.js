dff2cd010dada2e2f57cce440c3d56d09467d5aa
Pedro Morais <pedro.morais1997@gmail.com>
2020-01-30 16:55:21 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+
 const routes = require('./routes');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const PORT = 9999
+const PORT = ;
 app.listen(PORT, () => {
     console.log(`ONLINE NA PORTA ${PORT}`)
 });
