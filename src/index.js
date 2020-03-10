edf5881391696c55ffc71b3ef7ec44225bd0c7cb
pedromoraisf <pedro.morais1997@gmail.com>
2020-03-10 08:31:17 -0300

    fix: algumas configurações no entry point

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var cors = require('cors');
-var app = express();
-
-const routes = require('./routes');
+const express = require('express'), 
+    cors = require('cors'), 
+    app = express(), 
+    routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const PORT = 9999;
-app.listen(PORT, () => {
-    console.log(`ONLINE NA PORTA ${PORT}`);
-});
+const PORT = 11111,
+    HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => console.log(`ONLINE NA PORTA ${PORT}`));
